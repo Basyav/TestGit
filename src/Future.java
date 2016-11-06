363e413c68afca5b43c4499017bc9558bea98c7e
Basyav <zbas@mail.ru>
2016-11-06 14:02:54 +0300

    5=9

--- src/Future.java
+++ src/Future.java
@@ -3,5 +3,6 @@ public class Future {
     public static void main(String[] args) {
         System.out.print("Future");
         System.out.print("3+2");
+        System.out.println("5=9");
     }
 }
